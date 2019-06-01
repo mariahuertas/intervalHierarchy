51b1535ed21d3320186ff1fd3579e8e7a7745285
mariahuertas <mariahuertas296@gmail.com>
2019-06-01 14:23:26 +0200

    Add new tests of opened-closed intervals in OpenedClosedIntervalTest

--- src/test/java/tdd/intervalHierarchy/OpenedClosedIntervalTest.java
+++ src/test/java/tdd/intervalHierarchy/OpenedClosedIntervalTest.java
@@ -27,4 +27,46 @@ public class OpenedClosedIntervalTest {
 		assertFalse(one.isIntersected(another));
 	}
 	
+	@Test
+	public void testIsIntersectedOverlapingByEquals() {
+		Interval one = new IntervalBuilder().min(3).max(14).fromIsClosed(false).build();
+		Interval another = new IntervalBuilder().min(3).max(14).untilIsClosed(true).build();
+		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedOverlapingByRight() {
+		Interval one = new IntervalBuilder().min(3).max(14).fromIsClosed(false).build();
+		Interval another = new IntervalBuilder().min(7).max(17).untilIsClosed(true).build();
+		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedOverlapingByBoth() {
+		Interval one = new IntervalBuilder().min(3).max(14).fromIsClosed(false).build();
+		Interval another = new IntervalBuilder().min(0).max(17).untilIsClosed(true).build();
+		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedOverlapingByInside() {
+		Interval one = new IntervalBuilder().min(3).max(14).fromIsClosed(false).build();
+		Interval another = new IntervalBuilder().min(5).max(10).untilIsClosed(true).build();
+		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedNotOverlapingByLeft() {
+		Interval one = new IntervalBuilder().min(3).max(14).fromIsClosed(false).build();
+		Interval another = new IntervalBuilder().min(0).max(2).untilIsClosed(true).build();
+		assertFalse(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedNotOverlapingByRight() {
+		Interval one = new IntervalBuilder().min(3).max(14).fromIsClosed(false).build();
+		Interval another = new IntervalBuilder().min(16).max(22).untilIsClosed(true).build();
+		assertFalse(one.isIntersected(another));
+	}
+	
 }
