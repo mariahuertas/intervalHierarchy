2486c940b12857067d0e0d4944091cb732cec5c8
mariahuertas <mariahuertas296@gmail.com>
2019-06-01 13:48:37 +0200

    Modify conditional expression in UntilEndPoint

--- src/main/java/tdd/intervalHierarchy/UntilEndPoint.java
+++ src/main/java/tdd/intervalHierarchy/UntilEndPoint.java
@@ -5,11 +5,8 @@ public class UntilEndPoint extends Point {
 	UntilEndPoint(double point, boolean isClosed) {
 		super(point, isClosed);
 	}
-	
+
 	public boolean isRight(Point another){
-		if ((this.pointValue > another.getPointValue())|| this.isTheSame(another)) {
-			return true;
-		}
-		return false;
+		return (this.pointValue > another.getPointValue())|| this.isTheSame(another);
 	}
 }
